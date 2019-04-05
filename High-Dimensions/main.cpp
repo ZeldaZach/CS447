c6cffc3c69108f39f992c218e2457fa97f66befa
Zach Halpern <ZaHalpern+github@gmail.com>
2019-04-05 01:22:51 -0400

    More imrpvements based on new paper

--- High-Dimensions/main.cpp
+++ High-Dimensions/main.cpp
@@ -13,43 +13,52 @@ namespace plt = matplotlibcpp;
  * of a sphere in K dimension
  * @param dimension Dimension to test
  * @param sample_points How many points to generate on sphere
+ * @param is_parallel to execute in OpenMP or not
  * @return Histogram vector
  */
-std::vector<int> sample_sphere(const int dimension, const int sample_points)
+std::vector<int> sample_sphere(const int dimension, const int sample_points, const bool is_parallel)
 {
     assert(dimension > 0);
 
-    std::default_random_engine generator; //(std::chrono::system_clock::now().time_since_epoch().count());
+    std::default_random_engine generator(std::chrono::system_clock::now().time_since_epoch().count());
     std::normal_distribution<> distribution(0.0, 1.0);
 
     std::vector<int> histogram(100);
 
     const auto begin = std::chrono::steady_clock::now();
 
-#pragma omp parallel for shared(histogram)
-    // Run the experiment N times
+// http://compneuro.uwaterloo.ca/files/publications/voelker.2017.pdf
+#pragma omp parallel for num_threads(omp_get_max_threads()) shared(histogram) if (is_parallel)
     for (int i = 0; i < sample_points; i++) {
-        double sum;
-        std::vector<double> gaussian_values(dimension);
+        double normalizer = 0.0, sum = 0.0;
+        std::vector<double> gaussian_values(dimension + 2);
 
-        do {
-            // Determine our Gaussian variables
-            gaussian_values.clear();
-            sum = 0;
+        for (int d = 0; d < dimension + 2; d++) {
+            gaussian_values.push_back(distribution(generator));
+            normalizer += gaussian_values.back() * gaussian_values.back();
+        }
+        gaussian_values.pop_back();
+        gaussian_values.pop_back();
 
-            for (auto j = 0; j < dimension; j++) {
-                const double random_num = distribution(generator);
+        normalizer = std::sqrt(normalizer);
+        for (auto &value : gaussian_values) {
+            value /= normalizer;
+            sum += value * value;
+        }
 
-                gaussian_values.push_back(random_num);
-                sum += random_num * random_num;
-            }
-        } while (sum > 1);
-
+        sum = std::sqrt(sum);
         ++histogram[static_cast<int>(std::sqrt(sum) * 100)];
     }
-
     const auto end = std::chrono::steady_clock::now();
-    std::cout << "Time to build dimension " << dimension << " with " << sample_points
+
+    // Print out timing results
+    if (is_parallel) {
+        std::cout << " (Parallel x" << omp_get_max_threads() << ") ";
+    } else {
+        std::cout << "(Not Parallel) ";
+    }
+
+    std::cout << "Dimension " << dimension << " with " << sample_points
               << " nodes: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "ms"
               << std::endl;
 
@@ -77,17 +86,41 @@ int main(int argc, char **argv)
 
     std::cout << "Using " << omp_get_max_threads() << " threads" << std::endl;
 
-    // Calculate and plot the histogram
-    for (int dimension = 2; dimension <= 9; dimension++) {
-        const auto histogram = sample_sphere(dimension, node_points);
-        plt::named_plot(std::to_string(dimension), histogram);
-    }
+    // Holder to show the histogram
+    std::vector<std::vector<int>> histograms(14);
 
-    // More plot stuff
+    // Calculate histogram for non-parallel for timing
+    const auto start = std::chrono::steady_clock::now();
+    for (int dimension = 2; dimension <= 16; dimension++) {
+        const auto histogram = sample_sphere(dimension, node_points, false);
+        histograms.push_back(histogram);
+    }
+    const auto end = std::chrono::steady_clock::now();
+
+    // Clear histogram for real insertions
+    histograms.clear();
+
+    // Calculate and plot the histogram (for parallel version)
+    const auto p_start = std::chrono::steady_clock::now();
+    for (int dimension = 2; dimension <= 16; dimension++) {
+        const auto histogram = sample_sphere(dimension, node_points, true);
+        histograms.push_back(histogram);
+    }
+    const auto p_end = std::chrono::steady_clock::now();
+
+    // Print timetables
+    std::cout << "Non-Parallel computation time: "
+              << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
+    std::cout << "    Parallel computation time: "
+              << std::chrono::duration_cast<std::chrono::milliseconds>(p_end - p_start).count() << "ms" << std::endl;
+
+    // Create a histogram plot setup
+    for (unsigned long i = 0; i < histograms.size(); i++) {
+        plt::named_plot(std::to_string(i + 2), histograms.at(i));
+    }
     plt::title("Hypersphere Point Check");
     plt::xlabel("Distance from Sphere Center * 100");
     plt::ylabel("Points at Specific Distance");
     plt::legend();
-    plt::clf();
-    // plt::show();
+    plt::show();
 }
